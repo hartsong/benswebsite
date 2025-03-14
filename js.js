9fe34d127c8f4f329a2ac6ac68b1205c758d1128
Benjamin <alanhartmusic@gmail.com>
2025-03-14 15:11:50 -0500

    password!

--- js.js
+++ js.js
@@ -1,13 +1,24 @@
-let password = prompt("put a password in here");
-let answer = 4938
+// let password = prompt("put a password in here");
+// let answer = 4938
 
-if(password == answer){
-    console.log("It works!")
-}
-else{
-    alert("lets see if this works!")
-}
+// if(password == answer){
+//     console.log("It works!")
+// }
+// else{
+//     alert("lets see if this works!")
+// }
 
-for(let i = 1; i <= 100000; i++){
-    console.log(i);
+// for(let i = 1; i <= 100; i++){
+//     console.log("ofod");
+//     console.log("olhel (olel)");
+//     console.log("rfo");
+//     console.log("ofod");
+// }
+const password = 4938;
+let guess = prompt("enter the password");
+while (guess != password){
+            prompt("Wrong password! Try again");
+}
+if (guess == password){
+ alert("you did it!")
 }

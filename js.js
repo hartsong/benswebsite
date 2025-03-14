226f2b20b57fa8abcb402792169d76aadab370b3
Benjamin <alanhartmusic@gmail.com>
2025-03-14 16:01:37 -0500

    more passwords

--- js.js
+++ js.js
@@ -14,11 +14,36 @@
 //     console.log("rfo");
 //     console.log("ofod");
 // }
-const password = 4938;
+//first
+
+const password = 8076;
+ for(let breakPsw = 940583950385949;guess = breakPsw || try2 == breakPsw || try4 == breakPsw; ){break;}
 let guess = prompt("enter the password");
 while (guess != password){
             prompt("Wrong password! Try again");
 }
-if (guess == password){
- alert("you did it!")
+
+//second
+if(guess = password){
+const trying = 4938;
+
+let try2 = prompt("enter the code");
+while (try2 != trying) {
+            prompt("Wrong code! Try again");
 }
+
+
+
+//third
+if(try2 = trying){
+    const trying3 = 4938;
+    
+    let try4 = prompt("enter the passcode");
+    while (try4 != trying3) {
+                prompt("Wrong passcode! Try again");
+    }
+if (try4 == trying3) {
+ alert("explore!");
+}
+}
+

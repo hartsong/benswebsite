f31dec39f41086f0c2ea8a9fb38e6c415a2a170b
Benjamin <alanhartmusic@gmail.com>
2025-09-26 18:37:06 -0500

    Version: 1.30

--- app.js
+++ app.js
@@ -2,25 +2,16 @@
 function protectpasscode() {
     const result = document.getElementById("tutorial").value;
     let passcode = "35029";result.att
-    let space = " ";
-    if (result == space) {
+    let space = "";
+    let space2 = " "
+    if ((result == space) ||(result == space2)) {
         alert("Please don't do that."); location.reload();
     } else {
         if (result == passcode) {
             window.location.href = "Code2.html";
         } else {
-            const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
 
-            const alert = (message, type) => {
-                const wrapper = document.createElement('div')
-                wrapper.innerHTML = [
-                    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
-                    `   <div>${message}</div>`,
-                    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
-                    '</div>'
-                ].join('')
-
-                alertPlaceholder.append(wrapper)
+alert("Access denied.")
             }
 
             const alertTrigger = document.getElementById('liveAlertBtn')
@@ -32,4 +23,4 @@ function protectpasscode() {
             }
         }
     }
-}
+

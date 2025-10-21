ea1793a2ddf1caff333e0d2ccb999af2c47a0a57
Benjamin <alanhartmusic@gmail.com>
2025-10-21 17:49:03 -0500

    Added Top secret website and place to get there

---
+++ Ts.js
@@ -0,0 +1,15 @@
+
+function p() {
+    let p = prompt("Enter code")
+    if (p === "52234") {
+        document.getElementById("tops").innerText = "Access Allowed"
+
+    } if ((p == "") ||(p == " ")) {
+        alert("Hey, enter a code or press cancel")
+        p()
+    } 
+    
+    if ((p !== "52234") && (p !== null)) {
+        alert("Acess denied")
+    }
+}

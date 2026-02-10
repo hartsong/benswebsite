f3e5fdb9d52328a74c51884701c2b88fe3fceeeb
Benjamin <alanhartmusic@gmail.com>
2026-02-09 18:21:38 -0600

    Database stuff, needs debugging

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
+
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1:27017/Website')
+    .then(() => console.log("Connection open!!!"))
+    .catch(err => console.log("Oh no error!!!", err));
+mongoose.set('strictQuery', true);
+
+
+const password = Website.Data.find({ name: "Password1" });
+console.log(password);
+
 function protectpasscode() {
     const result = document.getElementById("tutorial").value;
     let passcode = "16840";result.att
@@ -11,7 +23,7 @@ function protectpasscode() {
         }
 if (result == "52234") {
             window.location.href = "tops.html";
-            alert("Welcome back to the TOP SECRET area!");
+            alert("Welcome to the TOP SECRET area!");
 }
         else {
 
